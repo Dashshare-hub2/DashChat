2f645dadb8623da04f41b1d90f202568c8ac0beb
Daianhnguyen <dashshare@proton.me>
2026-08-14 21:35:11 +0700

    edit mod

--- src/ChatManager.cpp
+++ src/ChatManager.cpp
@@ -1,105 +1,83 @@
-#pragma once
+
 #include <sio_client.h>
+
 #ifdef WIN32
+    #undef ERROR
     #undef OPTIONAL
     #undef DELETE
     #undef min
     #undef max
+    #undef BYTE
 #endif
+
+#include "ChatManager.hpp"
 #include <Geode/Geode.hpp>
-#include <string>
-#include <functional>
 
 using namespace geode::prelude;
 
-struct ChatMessage {
-    std::string discordName;
-    std::string gdUser;
-    std::string avatarUrl;
-    std::string message;
-    std::string room;
-};
+ChatManager::ChatManager() {
+    m_client = std::make_unique<sio::client>();
+}
 
-class ChatManager {
-private:
-    sio::client m_client;
-    bool m_connected = false;
-    std::string m_currentRoom = "global";
-    std::string m_discordName = "";
-    std::string m_avatarUrl = "";
-
-    std::function<void(const ChatMessage&)> m_onMessageCallback;
-    std::function<void(const std::string&, const std::string&, const std::string&)> m_onInviteCallback;
-
-public:
-    static ChatManager* get() {
-        static ChatManager instance;
-        return &instance;
+ChatManager::~ChatManager() {
+    if (m_client) {
+        m_client->sync_close();
+        m_client->clear_con_listeners();
     }
+}
 
-    void connect(const std::string& serverUrl) {
-        m_client.set_open_listener([this]() {
-            m_connected = true;
-            log::info("Connected to DashChat Server!");
-        });
+ChatManager* ChatManager::get() {
+    static ChatManager instance;
+    return &instance;
+}
 
-        m_client.socket()->on("receive-message", sio::sio_message::list_cb([this](const sio::event& ev) {
-            auto data = ev.get_message()->get_map();
-            ChatMessage msg;
-            msg.discordName = data["discordName"]->get_string();
-            msg.gdUser = data["gdUser"]->get_string();
-            msg.avatarUrl = data["avatarUrl"]->get_string();
-            msg.message = data["message"]->get_string();
-            msg.room = data["room"]->get_string();
+void ChatManager::connect(const std::string& serverUrl) {
+    if (m_connected) return;
 
-            Loader::get()->queueInMainThread([this, msg]() {
-                if (m_onMessageCallback) m_onMessageCallback(msg);
-            });
-        }));
+    m_client->set_open_listener([this]() {
+        m_connected = true;
+        log::info("Connected to DashChat server!");
+    });
 
-        m_client.socket()->on("player-invited", sio::sio_message::list_cb([this](const sio::event& ev) {
-            auto data = ev.get_message()->get_map();
-            std::string inviterGD = data["inviterGD"]->get_string();
-            std::string targetGD = data["targetGD"]->get_string();
-            std::string roomCode = data["roomCode"]->get_string();
+    m_client->set_close_listener([this](sio::client::close_reason const& reason) {
+        m_connected = false;
+        log::info("Disconnected from DashChat server.");
+    });
 
-            Loader::get()->queueInMainThread([this, inviterGD, targetGD, roomCode]() {
-                if (m_onInviteCallback) m_onInviteCallback(inviterGD, targetGD, roomCode);
-            });
-        }));
+    m_client->set_fail_listener([this]() {
+        m_connected = false;
+        log::error("Failed to connect to DashChat server.");
+    });
 
-        m_client.connect(serverUrl);
+    m_client->socket()->on("chat_message", sio::socket::event_listener_aux([this](std::string const& name, sio::message::ptr const& data, bool hasAck, sio::message::list &ack_resp) {
+        if (data && data->get_flag() == sio::message::flag_object) {
+            auto obj = data->get_map();
+            if (obj.find("sender") != obj.end() && obj.find("message") != obj.end()) {
+                std::string sender = obj["sender"]->get_string();
+                std::string message = obj["message"]->get_string();
+
+                Loader::get()->queueInMainThread([this, sender, message]() {
+                    m_messages.push_back({sender, message});
+                });
+            }
+        }
+    }));
+
+    m_client->connect(serverUrl);
+}
+
+void ChatManager::disconnect() {
+    if (m_client && m_connected) {
+        m_client->close();
+        m_connected = false;
     }
+}
 
-    void authenticateToken(const std::string& token, const std::string& gdUser) {
-        sio::message::ptr data = sio::object_message::create();
-        data->get_map()["authToken"] = sio::string_message::create(token);
-        data->get_map()["gdUser"] = sio::string_message::create(gdUser);
-        m_client.socket()->emit("authenticate", data);
-    }
+void ChatManager::sendMessage(const std::string& msg) {
+    if (!m_connected || !m_client) return;
 
-    void joinRoom(const std::string& roomCode) {
-        m_client.socket()->emit("join-room", roomCode);
-        m_currentRoom = roomCode;
-    }
+    sio::message::ptr pack = sio::object_message::create();
+    pack->get_map()["message"] = sio::string_message::create(msg);
 
-    void joinGlobal() {
-        m_client.socket()->emit("join-global");
-        m_currentRoom = "global";
-    }
-
-    void sendMessage(const std::string& text) {
-        m_client.socket()->emit("send-message", text);
-    }
-
-    void sendInvite(const std::string& targetGD, const std::string& roomCode) {
-        sio::message::ptr data = sio::object_message::create();
-        data->get_map()["targetGD"] = sio::string_message::create(targetGD);
-        data->get_map()["roomCode"] = sio::string_message::create(roomCode);
-        m_client.socket()->emit("invite-player", data);
-    }
-
-    void setOnMessageCallback(std::function<void(const ChatMessage&)> cb) { m_onMessageCallback = cb; }
-    void setOnInviteCallback(std::function<void(const std::string&, const std::string&, const std::string&)> cb) { m_onInviteCallback = cb; }
-    std::string getCurrentRoom() const { return m_currentRoom; }
-};
+    m_client->socket()->emit("send_message", pack);
+}
--- src/ChatManager.hpp
+++ src/ChatManager.hpp
@@ -1,98 +1,35 @@
 #pragma once
+
 #include <Geode/Geode.hpp>
 #include <string>
-#include <functional>
+#include <vector>
+#include <memory>
 
-using namespace geode::prelude;
+namespace sio {
+    class client;
+}
 
 struct ChatMessage {
-    std::string discordName;
-    std::string gdUser;
-    std::string avatarUrl;
+    std::string sender;
     std::string message;
-    std::string room;
 };
 
 class ChatManager {
 private:
-    sio::client m_client;
+    std::unique_ptr<sio::client> m_client;
     bool m_connected = false;
-    std::string m_currentRoom = "global";
-    std::string m_discordName = "";
-    std::string m_avatarUrl = "";
+    std::vector<ChatMessage> m_messages;
 
-    std::function<void(const ChatMessage&)> m_onMessageCallback;
-    std::function<void(const std::string&, const std::string&, const std::string&)> m_onInviteCallback;
+    ChatManager();
+    ~ChatManager();
 
 public:
-    static ChatManager* get() {
-        static ChatManager instance;
-        return &instance;
-    }
+    static ChatManager* get();
 
-    void connect(const std::string& serverUrl) {
-        m_client.set_open_listener([this]() {
-            m_connected = true;
-            log::info("Connected to DashChat Server!");
-        });
-
-        m_client.socket()->on("receive-message", sio::sio_message::list_cb([this](const sio::event& ev) {
-            auto data = ev.get_message()->get_map();
-            ChatMessage msg;
-            msg.discordName = data["discordName"]->get_string();
-            msg.gdUser = data["gdUser"]->get_string();
-            msg.avatarUrl = data["avatarUrl"]->get_string();
-            msg.message = data["message"]->get_string();
-            msg.room = data["room"]->get_string();
-
-            Loader::get()->queueInMainThread([this, msg]() {
-                if (m_onMessageCallback) m_onMessageCallback(msg);
-            });
-        }));
-
-        m_client.socket()->on("player-invited", sio::sio_message::list_cb([this](const sio::event& ev) {
-            auto data = ev.get_message()->get_map();
-            std::string inviterGD = data["inviterGD"]->get_string();
-            std::string targetGD = data["targetGD"]->get_string();
-            std::string roomCode = data["roomCode"]->get_string();
-
-            Loader::get()->queueInMainThread([this, inviterGD, targetGD, roomCode]() {
-                if (m_onInviteCallback) m_onInviteCallback(inviterGD, targetGD, roomCode);
-            });
-        }));
-
-        m_client.connect(serverUrl);
-    }
-
-    void authenticateToken(const std::string& token, const std::string& gdUser) {
-        sio::message::ptr data = sio::object_message::create();
-        data->get_map()["authToken"] = sio::string_message::create(token);
-        data->get_map()["gdUser"] = sio::string_message::create(gdUser);
-        m_client.socket()->emit("authenticate", data);
-    }
-
-    void joinRoom(const std::string& roomCode) {
-        m_client.socket()->emit("join-room", roomCode);
-        m_currentRoom = roomCode;
-    }
-
-    void joinGlobal() {
-        m_client.socket()->emit("join-global");
-        m_currentRoom = "global";
-    }
-
-    void sendMessage(const std::string& text) {
-        m_client.socket()->emit("send-message", text);
-    }
-
-    void sendInvite(const std::string& targetGD, const std::string& roomCode) {
-        sio::message::ptr data = sio::object_message::create();
-        data->get_map()["targetGD"] = sio::string_message::create(targetGD);
-        data->get_map()["roomCode"] = sio::string_message::create(roomCode);
-        m_client.socket()->emit("invite-player", data);
-    }
-
-    void setOnMessageCallback(std::function<void(const ChatMessage&)> cb) { m_onMessageCallback = cb; }
-    void setOnInviteCallback(std::function<void(const std::string&, const std::string&, const std::string&)> cb) { m_onInviteCallback = cb; }
-    std::string getCurrentRoom() const { return m_currentRoom; }
+    void connect(const std::string& serverUrl);
+    void disconnect();
+    void sendMessage(const std::string& msg);
+    
+    const std::vector<ChatMessage>& getMessages() const { return m_messages; }
+    bool isConnected() const { return m_connected; }
 };
