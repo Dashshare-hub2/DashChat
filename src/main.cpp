4bab8ded5648b5ea54fb84029640f807c13bbe48
Daianhnguyen <dashshare@proton.me>
2026-08-19 10:07:08 +0700

    add Chat in Pause Level

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/PauseLayer.hpp> 
 #include <Geode/loader/SettingEvent.hpp>
 
 #include "ChatManager.hpp"
@@ -7,18 +8,14 @@
 
 using namespace geode::prelude;
 
-
 class $modify(DashChatMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
 
- 
         ChatManager::get()->connect();
 
         auto spr = CCSprite::createWithSpriteFrameName("GJ_chatBtn_001.png");
-        if (!spr) {
-            spr = ButtonSprite::create("Chat");
-        }
+        if (!spr) spr = ButtonSprite::create("Chat");
 
         auto btn = CCMenuItemSpriteExtra::create(
             spr, this, menu_selector(DashChatMenuLayer::onOpenDashChat)
@@ -39,13 +36,43 @@ class $modify(DashChatMenuLayer, MenuLayer) {
     }
 };
 
+class $modify(DashChatPauseLayer, PauseLayer) {
+    void customSetup() {
+        PauseLayer::customSetup();
+
+        auto spr = CCSprite::createWithSpriteFrameName("GJ_chatBtn_001.png");
+        if (!spr) spr = ButtonSprite::create("Chat");
+
+        auto btn = CCMenuItemSpriteExtra::create(
+            spr, this, menu_selector(DashChatPauseLayer::onOpenDashChat)
+        );
+
+        auto rightMenu = this->getChildByID("right-button-menu");
+        if (!rightMenu) {
+            rightMenu = this->getChildByID("bottom-right-menu");
+        }
+
+        if (rightMenu) {
+            rightMenu->addChild(btn);
+            rightMenu->updateLayout();
+        } else {
+            auto menu = CCMenu::create();
+            menu->setPosition({CCDirector::sharedDirector()->getWinSize().width - 40.f, 100.f});
+            menu->addChild(btn);
+            this->addChild(menu);
+        }
+    }
+
+    void onOpenDashChat(CCObject*) {
+        std::string inviteCode = Mod::get()->getSettingValue<std::string>("invite-code");
+        ChatLayer::create(inviteCode)->show();
+    }
+};
+
 $execute {
     listenForSettingChanges("enable-chat", [](bool enabled) {
-        if (enabled) {
-            ChatManager::get()->connect();
-        } else {
-            ChatManager::get()->disconnect();
-        }
+        if (enabled) ChatManager::get()->connect();
+        else ChatManager::get()->disconnect();
     });
 
     listenForSettingChanges("server-url", [](std::string const&) {
