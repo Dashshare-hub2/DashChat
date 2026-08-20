ed19ba06ab4fa55a29b5c6a122fccdad796345df
Daianhnguyen <dashshare@proton.me>
2026-08-20 16:21:14 +0700

    fix

--- src/hooks/PlayLayer.cpp
+++ src/hooks/PlayLayer.cpp
@@ -1,6 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <geode.custom-keybinds/include/Keybinds.hpp>
+#include <Keybinds.hpp>
 #include "../UI/ChatOverlay.hpp"
 
 using namespace geode::prelude;
