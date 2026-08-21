22ef8fecc097ca95ab9acb3fc09eac1ed040bc58
Daianhnguyen <dashshare@proton.me>
2026-08-21 16:52:30 +0700

    fix

--- src/UI/ChatCell.cpp
+++ src/UI/ChatCell.cpp
@@ -1,10 +1,12 @@
+#include <Geode/Geode.hpp>
+#include <Geode/utils/web.hpp>
 #include "ChatCell.hpp"
 
 using namespace geode::prelude;
 
-ChatCell* ChatCell::create() {
+ChatCell* ChatCell::create(std::string const& sender, std::string const& text, cocos2d::ccColor3B color) {
     auto ret = new ChatCell();
-    if (ret && ret->init()) {
+    if (ret && ret->init(sender, text, color)) {
         ret->autorelease();
         return ret;
     }
@@ -12,7 +14,7 @@ ChatCell* ChatCell::create() {
     return nullptr;
 }
 
-bool ChatCell::init() {
+bool ChatCell::init(std::string const& sender, std::string const& text, cocos2d::ccColor3B color) {
     if (!CCNode::init()) return false;
     return true;
 }
@@ -20,31 +22,30 @@ bool ChatCell::init() {
 void ChatCell::loadDiscordAvatar(CCNode* parentNode, std::string const& avatarUrl) {
     if (avatarUrl.empty() || !parentNode) return;
 
-    m_avatarListener.bind([parentNode](web::WebResponseEvent* event) {
-        if (auto response = event->getValue()) {
-            if (response->ok()) {
-                auto data = response->data();
-                if (!data.empty() && parentNode) {
-                    auto image = new CCImage();
-                    if (image->initWithImageData(const_cast<uint8_t*>(data.data()), data.size())) {
-                        auto texture = new CCTexture2D();
-                        if (texture->initWithImage(image)) {
-                            auto sprite = CCSprite::createWithTexture(texture);
-                            if (sprite) {
-                                float scale = 20.0f / sprite->getContentSize().width;
-                                sprite->setScale(scale);
-                                sprite->setPosition({ 15.0f, 15.0f });
-                                parentNode->addChild(sprite);
-                            }
-                            texture->release();
+    web::WebRequest req;
+
+    m_avatarListener.bind([parentNode](web::WebResponse* response) {
+        if (response && response->ok()) {
+            auto data = response->data();
+            if (!data.empty() && parentNode) {
+                auto image = new CCImage();
+                if (image->initWithImageData(const_cast<uint8_t*>(data.data()), data.size())) {
+                    auto texture = new CCTexture2D();
+                    if (texture->initWithImage(image)) {
+                        auto sprite = CCSprite::createWithTexture(texture);
+                        if (sprite) {
+                            float scale = 20.0f / sprite->getContentSize().width;
+                            sprite->setScale(scale);
+                            sprite->setPosition({ 15.0f, 15.0f });
+                            parentNode->addChild(sprite);
                         }
-                        image->release();
+                        texture->release();
                     }
+                    image->release();
                 }
             }
         }
     });
 
-    web::WebRequest req;
     m_avatarListener.setFilter(req.get(avatarUrl));
 }
--- src/UI/ChatCell.hpp
+++ src/UI/ChatCell.hpp
@@ -1,14 +1,13 @@
 #pragma once
 #include <Geode/Geode.hpp>
-#include <Geode/utils/async.hpp>
+#include <Geode/utils/web.hpp>
 
-class ChatCell : public cocos2d::CCNode { 
+class ChatCell : public cocos2d::CCNode {
 private:
-    geode::EventListener<geode::utils::web::WebResponseFilter> m_avatarListener;
-    cocos2d::CCLayerColor* m_bg = nullptr;
-    cocos2d::CCLabelBMFont* m_label = nullptr;
+    geode::TaskListener<geode::utils::web::WebResponse> m_avatarListener;
 
 public:
     static ChatCell* create(std::string const& sender, std::string const& text, cocos2d::ccColor3B color);
     bool init(std::string const& sender, std::string const& text, cocos2d::ccColor3B color);
+    void loadDiscordAvatar(cocos2d::CCNode* parentNode, std::string const& avatarUrl);
 };
