24b51f02a8aeed392a67304e9151192b6ba004c1
Daianhnguyen <dashshare@proton.me>
2026-08-22 21:58:30 +0700

    Save ChatOverlay.hpp.

--- src/UI/ChatOverlay.hpp
+++ src/UI/ChatOverlay.hpp
@@ -5,21 +5,26 @@
 #include <Geode/ui/TextInput.hpp>
 #include <Geode/cocos/extensions/GUI/CCScrollView/CCScrollView.h>
 
-using namespace geode::prelude;
-
-// Geode v3+ Popup CRTP: Pass class name itself as the 1st template arg
-class ChatOverlay : public geode::Popup<ChatOverlay, std::string const&> {
+class ChatOverlay : public geode::Popup<std::string const&> {
 private:
     cocos2d::CCNode* m_chatContainer = nullptr;
-    TextInput* m_inputNode = nullptr;
+    geode::TextInput* m_inputNode = nullptr;
     cocos2d::extension::CCScrollView* m_scrollView = nullptr;
     float m_chatHeight = 0.0f;
+    bool m_isTyping = false;
 
-
-    bool setup(std::string const& roomName);
+    bool setup(std::string const& roomName) override;
     void onSend(cocos2d::CCObject* sender);
 
 public:
     static ChatOverlay* create(std::string const& roomName);
     void addChatMessage(std::string const& sender, std::string const& text, std::string const& avatarUrl);
+    
+    bool isTyping() const { return m_isTyping; }
+    void toggleTyping(bool typing) { 
+        m_isTyping = typing; 
+        if (m_inputNode) {
+            m_inputNode->setTouchEnabled(typing);
+        }
+    }
 };
